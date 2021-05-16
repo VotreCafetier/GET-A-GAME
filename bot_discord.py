48223e5ce28efa4424ace8699ad94d78aabe2169
Dany Gauthier <danygauthier57@yahoo.ca>
2021-05-15 20:46:40 -0400

    Added json (yet again)

--- bot_discord.py
+++ bot_discord.py
@@ -2,17 +2,12 @@ import random
 import discord
 import json
 
-
-api_token = "NDY5NTkwODM4NjM3NDk0Mjc0.W1DqjA.oyh7oyUOQxWeAHAB3lNkJe9_eLo"
-
-with open('games.json') as f:
-  data_json = json.load(f)
-
-
-games_list = data_json
-
+token = "NDY5NTkwODM4NjM3NDk0Mjc0.W1DqjA.oyh7oyUOQxWeAHAB3lNkJe9_eLo"
 client = discord.Client()
 
+# Open games from json
+with open('games.json') as f:
+  games_list = json.load(f)
 
 @client.event
 async def on_ready():
@@ -20,15 +15,37 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    # Prevent the bot to reply to itself
     if message.author == client.user:
         return
 
+    # Generate a random games
     if message.content.startswith('$Gen'):
         await message.channel.send(random.choice(games_list))
 
+    # List all of the array
     if message.content.startswith('$List'):
         for games in games_list:
-            await message.channel.send(games)
+            await message.channel.send(games, end = "\n")
+
+    # Add an element to the game array
+    if message.content.startswith('$Add'):
+        msg_list = str(message.content)[5:]
+        #RENDU ICI
+        with open("games.json", "r+") as file:
+            data = json.load(file)
+            data.update(msg_list)
+            file.seek(0)
+            json.dump(data, file)
+
+        
+        games_list.append(msg_list)
+
+        print("Added : "+msg_list)
+        # games.list.append(message)
+        await message.channel.send("Added : "+msg_list)
+
+    #reset array
 
 
-client.run(api_token)
+client.run(token)
